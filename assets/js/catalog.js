f2230733ef87c6dd48a521f90475a561f6a9a868
akashraju4 <akash.raju@outlook.com>
2020-02-27 21:56:23 -0500

    update modal

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -159,14 +159,53 @@ $(document).ready(() => {
               <img width="50%" height="50%" class="rounded-circle" src="${imageUrl}" style="object-fit: cover; text-align: center" alt="Card image cap">
               <h5 class="card-title name">${userData.Name}</h5>
               <p class="card-text">${brand}</p>
+              <button id="modalbutton">Open Modal</button>
             </div>
             <div class="card-footer">
             </div>
           </div>
         </div>
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+          <!-- Modal content -->
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <img width="50%" height="50%" class="rounded-circle" src="${imageUrl}" style="object-fit: cover; text-align: center" alt="Card image cap">
+              <h5 class="card-title name">${userData.Name}</h5>
+              <p class="card-text">${brand}</p>
+            <iframe class="airtable-embed" src="https://airtable.com/embed/shrA6Bjw3qOoJhP2f?backgroundColor=green" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>      </div>
+        </div>
+        <script>
+        // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("modalbutton");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks on the button, open the modal
+            btn.onclick = function() {
+            modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+            modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+            }
+        </script>
       `;
     }
   
     startAirtable();
     // searchField.on("keyup", search);
   });
+
