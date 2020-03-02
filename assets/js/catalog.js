d091d8e10cfdbfc7c3fe6213052ee7d942dc898c
akashraju4 <akash.raju@outlook.com>
2020-03-02 11:28:09 -0500

    updates to UI

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -159,7 +159,7 @@ $(document).ready(() => {
               <img width="50%" height="50%" class="rounded-circle" src="${imageUrl}" style="object-fit: cover; text-align: center" alt="Card image cap">
               <h5 class="card-title name">${userData.Name}</h5>
               <p class="card-text">${brand}</p>
-              <button id="modalbutton">Open Modal</button>
+              <button id="modalbutton">Expand</button>
             </div>
             <div class="card-footer">
             </div>
@@ -168,13 +168,13 @@ $(document).ready(() => {
         <!-- The Modal -->
         <div id="myModal" class="modal">
           <!-- Modal content -->
-          <div class="modal-content">
+          <div class="modal-content" ">
             <span class="close">&times;</span>
             <img width="50%" height="50%" class="rounded-circle" src="${imageUrl}" style="object-fit: cover; text-align: center" alt="Card image cap">
               <h5 class="card-title name">${userData.Name}</h5>
               <p class="card-text">${brand}</p>
               <button id="learnmore">Learn About Product</button>
-            <iframe class="airtable-embed" src="https://airtable.com/embed/shrA6Bjw3qOoJhP2f?backgroundColor=green" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>      </div>
+              <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrA6Bjw3qOoJhP2f?backgroundColor=green" frameborder="0" onmousewheel="" width="100%" height="894" style="background: transparent; border: 1px solid #ccc;"></iframe>
         </div>
         <script>
         // Get the modal
